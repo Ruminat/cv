190b38e6c89eed9222ed86212d66c1356b1d95ec
ruminat <blowlyge@gmail.com>
2026-03-21 19:08:16 +0300

    keep it simple

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -3,7 +3,7 @@ import { text } from "../Typography";
 
 export function EducationPeriod({ period, specialty }: { period: string; specialty: string }) {
   return (
-    <Flex direction='column' gap={2}>
+    <Flex direction='column' gap={2} width='50%'>
       {text(period)}
       {text(specialty, { color: "hint", size: "small" })}
     </Flex>
--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -4,7 +4,7 @@ import { text } from "../Typography";
 export function Language({ language, level, icon }: { language: string; level: string; icon: string }) {
   return (
     <Flex alignItems='center' gap={2}>
-      <img src={icon} width='16px' /> {text(`${language} (${level})`)}
+      <img src={icon} width='16px' /> {text(`${language} — ${level}`)}
     </Flex>
   );
 }
